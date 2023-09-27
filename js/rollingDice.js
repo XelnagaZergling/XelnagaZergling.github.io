7bd95378dfb7db615410c516bdc719bd0de48e35
XelnagaZergling <114866519+XelnagaZergling@users.noreply.github.com>
2023-09-28 03:59:19 +0800

    Add files via upload

--- js/rollingDice.js
+++ js/rollingDice.js
@@ -1,59 +1,69 @@
-        // 显示范围
-        const numOfSidesInput = document.getElementById("numOfSides");
-        const numOfDiceInput = document.getElementById("numOfDice");
-        const modifierInput = document.getElementById("modifier");
-        const addRadio = document.getElementById("add");
-        const subtractRadio = document.getElementById("subtract");
-        const rangeSpan = document.getElementById("range");
+const numOfSidesInput = document.getElementById("numOfSides");
+const numOfDiceInput = document.getElementById("numOfDice");
+const modifierInput = document.getElementById("modifier");
+const addRadio = document.getElementById("add");
+const subtractRadio = document.getElementById("subtract");
+const rangeSpan = document.getElementById("range");
 
-        function displayRange() {
-            const numOfSides = parseInt(numOfSidesInput.value);
-            const numOfDice = parseInt(numOfDiceInput.value);
-            const modifierValue = modifierInput.value.trim() === '' ? 0 : parseInt(modifierInput.value);
-            const operation = addRadio.checked ? 1 : -1;
+function displayRange() {
+    const numOfSides = parseInt(numOfSidesInput.value);
+    const numOfDice = parseInt(numOfDiceInput.value);
+    const modifierValue = modifierInput.value.trim() === '' ? 0 : parseInt(modifierInput.value);
+    const operation = addRadio.checked ? 1 : -1;
 
-            let minResult = numOfDice;
-            let maxResult = numOfDice;
+    if (numOfSides <= 0 || numOfDice <= 0) {
+        alert("骰子的面数和数量必须大于0，请重新输入。");
+        // 清空输入框
+        numOfSidesInput.value = "6";
+        numOfDiceInput.value = "1";
+        modifierInput.value = "";
+        rangeSpan.textContent = "";
+        return;
+    }
 
-            if (numOfSides > 1) {
-                minResult *= 1;
-                maxResult *= numOfSides;
-            }
+    let minResult = numOfDice;
+    let maxResult = numOfDice;
 
-            minResult += modifierValue * operation;
-            maxResult += modifierValue * operation;
+    if (numOfSides > 1) {
+        minResult *= 1;
+        maxResult *= numOfSides;
+    }
 
-            if (minResult < 0 || maxResult < 0) {
-                alert("范围不能包含负数，请重新输入。");
-                // 清空输入框
-                modifierInput.value = "";
-                rangeSpan.textContent = "";
-                return;
-            }
+    minResult += modifierValue * operation;
+    maxResult += modifierValue * operation;
 
-            rangeSpan.textContent = `${minResult} - ${maxResult}`;
-        }
+    if (minResult < 0 || maxResult < 0) {
+        alert("范围不能包含负数，请重新输入。");
+        // 清空输入框
+        modifierInput.value = "";
+        rangeSpan.textContent = "";
+        return;
+    }
 
-        numOfSidesInput.addEventListener("input", displayRange);
-        numOfDiceInput.addEventListener("input", displayRange);
-        modifierInput.addEventListener("input", displayRange);
-        addRadio.addEventListener("change", displayRange);
-        subtractRadio.addEventListener("change", displayRange);
+    rangeSpan.textContent = `${minResult} - ${maxResult}`;
+}
 
-        // 投骰子
-        function rollDice() {
-            const numOfSides = parseInt(document.getElementById("numOfSides").value);
-            const numOfDice = parseInt(document.getElementById("numOfDice").value);
-            const modifierValue = modifierInput.value.trim() === '' ? 0 : parseInt(modifierInput.value);
-            const operation = addRadio.checked ? 1 : -1;
+numOfSidesInput.addEventListener("input", displayRange);
+numOfDiceInput.addEventListener("input", displayRange);
+modifierInput.addEventListener("input", displayRange);
+addRadio.addEventListener("change", displayRange);
+subtractRadio.addEventListener("change", displayRange);
 
-            const results = [];
 
-            for (let i = 0; i < numOfDice; i++) {
-                const roll = Math.floor(Math.random() * numOfSides) + 1;
-                results.push(roll);
-            }
+// 投骰子
+function rollDice() {
+    const numOfSides = parseInt(document.getElementById("numOfSides").value);
+    const numOfDice = parseInt(document.getElementById("numOfDice").value);
+    const modifierValue = modifierInput.value.trim() === '' ? 0 : parseInt(modifierInput.value);
+    const operation = addRadio.checked ? 1 : -1;
 
-            const totalResult = results.reduce((a, b) => a + b, 0) + modifierValue * operation;
-            document.getElementById("result").textContent = results.join(" + ") + ` ${addRadio.checked ? '+' : '-'} ${modifierValue} = ${totalResult}`;
-        }
+    const results = [];
+
+    for (let i = 0; i < numOfDice; i++) {
+        const roll = Math.floor(Math.random() * numOfSides) + 1;
+        results.push(roll);
+    }
+
+    const totalResult = results.reduce((a, b) => a + b, 0) + modifierValue * operation;
+    document.getElementById("result").textContent = results.join(" + ") + ` ${addRadio.checked ? '+' : '-'} ${modifierValue} = ${totalResult}`;
+}
